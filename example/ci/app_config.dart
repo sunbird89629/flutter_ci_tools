c329b9e4d343404733ebe43d7145ae676242cab4
Howard Wang <sunbird.work@gmail.com>
2026-05-20 22:00:21 +0800

    feat(example): add CI app config with env-var-driven credentials

---
+++ example/ci/app_config.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+
+import 'package:flutter_ci_tools/flutter_ci_tools.dart';
+
+const _placeholder = 'YOUR_VALUE_HERE';
+
+String _env(String key) => Platform.environment[key] ?? _placeholder;
+
+final exampleConfig = CIToolsConfig(
+  appName: 'FlutterCIToolsExample',
+  seedBuildNumber: 10000,
+  pgyerApiKey: _env('PGYER_API_KEY'),
+  feishuWebhookUrl: _env('FEISHU_WEBHOOK_URL'),
+);
+
+class ProdCredentials {
+  static String get googlePlayPackageName => _env('GOOGLE_PLAY_PACKAGE_NAME');
+  static String get googlePlayJsonKeyPath => _env('GOOGLE_PLAY_JSON_KEY_PATH');
+  static String get appStoreIssuerId => _env('APP_STORE_ISSUER_ID');
+  static String get appStoreApiKeyId => _env('APP_STORE_API_KEY_ID');
+  static String get appStoreApiKeyPath => _env('APP_STORE_API_KEY_PATH');
+}
