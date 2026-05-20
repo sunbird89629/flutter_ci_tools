791e299292197170012a8c7849c5846d1526a410
Howard Wang <sunbird.work@gmail.com>
2026-05-20 22:30:47 +0800

    chore(example): rename package to com.yolobuyer.test
    Change Android namespace/applicationId and iOS bundle identifier
    from com.example.flutter_ci_tools_example to com.yolobuyer.test,
    including Kotlin package directory restructuring.

--- example/android/app/build.gradle.kts
+++ example/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.flutter_ci_tools_example"
+    namespace = "com.yolobuyer.test"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.flutter_ci_tools_example"
+        applicationId = "com.yolobuyer.test"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- example/android/app/src/main/kotlin/com/yolobuyer/test/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/yolobuyer/test/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.flutter_ci_tools_example
+package com.yolobuyer.test
 
 import io.flutter.embedding.android.FlutterActivity
 
