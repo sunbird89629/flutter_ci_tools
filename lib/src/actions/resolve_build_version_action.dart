d901beb75788643f1cf647e40c70dbdfdd3ee4c6
Howard Wang <sunbird.work@gmail.com>
2026-05-23 20:25:27 +0800

    feat: add ResolveBuildVersionAction

---
+++ lib/src/actions/resolve_build_version_action.dart
@@ -0,0 +1,27 @@
+import '../logger.dart';
+import '../pipeline_context.dart';
+import '../version_manager.dart';
+import 'pipeline_action.dart';
+
+/// Computes the next build number via [VersionManager] and writes it to
+/// [PipelineContext.buildNumber].
+class ResolveBuildVersionAction extends PipelineAction<void> {
+  ResolveBuildVersionAction({VersionManager? versionManager})
+      : _versionManager = versionManager ?? DefaultVersionManager();
+
+  final VersionManager _versionManager;
+
+  @override
+  String get name => 'Resolve Build Version';
+
+  @override
+  Future<void> run(PipelineContext context) async {
+    final number = await _versionManager.computeNextBuildNumber(
+      context.config.seedBuildNumber,
+    );
+    context.buildNumber = number;
+    Logger.info(
+      'Resolved buildNumber=${context.buildNumber}  buildName=${context.buildName}',
+    );
+  }
+}
---
+++ test/actions/resolve_build_version_action_test.dart
@@ -0,0 +1,41 @@
+import 'package:flutter_ci_tools/src/actions/resolve_build_version_action.dart';
+import 'package:flutter_ci_tools/src/config.dart';
+import 'package:flutter_ci_tools/src/pipeline_context.dart';
+import 'package:flutter_ci_tools/src/version_manager.dart';
+import 'package:test/test.dart';
+
+class _FakeVersionManager implements VersionManager {
+  int nextBuildNumber = 12345;
+  int? receivedSeed;
+
+  @override
+  Future<int?> fetchLatestBuildNumber() async => null;
+
+  @override
+  Future<int> computeNextBuildNumber(int seed) async {
+    receivedSeed = seed;
+    return nextBuildNumber;
+  }
+
+  @override
+  Future<void> pushNewBuildTag(int buildNumber) async {}
+
+  @override
+  Future<void> interactiveBumpAndPush(int seed) async {}
+}
+
+void main() {
+  test('ResolveBuildVersionAction sets context.buildNumber from VersionManager', () async {
+    final version = _FakeVersionManager()..nextBuildNumber = 12001;
+    final action = ResolveBuildVersionAction(versionManager: version);
+    final context = PipelineContext(
+      config: const CIToolsConfig(appName: 'TestApp', seedBuildNumber: 12000),
+    );
+
+    await action.run(context);
+
+    expect(action.name, 'Resolve Build Version');
+    expect(version.receivedSeed, 12000);
+    expect(context.buildNumber, 12001);
+  });
+}
