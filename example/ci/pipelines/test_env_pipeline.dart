cf766e4e30cad6d564289902459cfa04af5d5806
Howard Wang <sunbird.work@gmail.com>
2026-06-09 20:41:48 +0800

    example: update TestEnvPipeline to use parallel upload

--- example/ci/pipelines/test_env_pipeline.dart
+++ example/ci/pipelines/test_env_pipeline.dart
@@ -2,6 +2,11 @@ import 'package:flutter_ci_tools/flutter_ci_tools.dart';
 
 import '../app_config.dart';
 
+final String pgyerApiKey = '1540c89d7f12ade530a14ac4adf9caa2';
+// MessageBus Bot Webhook
+final String feishuWebhookUrl =
+    'https://open.feishu.cn/open-apis/bot/v2/hook/82ab0b57-f8c9-493f-a69d-575271f12bfd';
+
 class TestEnvPipeline extends Pipeline {
   @override
   PipelineContext createContext(List<String> args) =>
@@ -23,48 +28,32 @@ Usage: dart run ci/build.dart test
     await runAction(CheckGitStatusAction());
     await runAction(CleanProjectAction());
 
-    await runAction(BuildAndroidAction(
+    // 构建，显式拿到两个文件
+    final androidFile = await runAction(BuildAndroidAction(
       envName: 'test',
       buildType: AndroidBuildType.apk,
     ));
-    await _deployToPgyer();
-
-    await runAction(BuildIOSAction(
+    final iosFile = await runAction(BuildIOSAction(
       envName: 'test',
       exportMethod: 'development',
     ));
-    await _deployToPgyer();
+
+    // 并行上传
+    final urls = await runParallel([
+      PgyerUploadV2Action(apiKey: pgyerApiKey, artifact: androidFile),
+      PgyerUploadV2Action(apiKey: pgyerApiKey, artifact: iosFile),
+    ]);
+
+    // 一条通知包含两个链接
+    await runAction(FeishuBuildNotifyAction(
+      webhookUrl: feishuWebhookUrl,
+      target: DeployTarget.pgyer,
+      downloadUrls: urls,
+    ));
 
     await runAction(PushBuildTagAction());
   }
 
   @override
   Future<void> afterBuild() => runAction(RestoreWorkspaceAction());
-
-  Future<void> _deployToPgyer() async {
-    final pgyerUrl = await runAction(PgyerUploadAction(
-      apiKey: (context as ExampleAppContext).pgyerApiKey,
-      description: await _pgyerDescription(),
-    ));
-    await runAction(FeishuBuildNotifyAction(
-      webhookUrl: (context as ExampleAppContext).feishuWebhookUrl,
-      target: DeployTarget.pgyer,
-      downloadUrl: pgyerUrl,
-    ));
-  }
-
-  Future<String> _pgyerDescription() async {
-    final git = context.git;
-    final gitHash = await git.getShortHash();
-    final recentCommits = await git.getRecentCommits(count: 15);
-    return [
-      'versionName: ${context.buildName}',
-      'versionCode: ${context.buildNumber}',
-      'env:         test',
-      'git_hash:    $gitHash',
-      '',
-      'recent commits:',
-      recentCommits,
-    ].join('\n');
-  }
 }
